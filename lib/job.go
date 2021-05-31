0ccd63797d47905fc2df28003ba6e22c4d08af4b
luo <506765498@qq.com>
2021-06-01 05:23:17 +0800

    modify getjob

--- lib/job.go
+++ lib/job.go
@@ -67,7 +67,7 @@ func GetJob(jobName string, jobParallelism int32, deleteJobAfterFinishSec int32,
 							Command: []string{"/bin/sh", "-c"},
 							Args: []string{"/entyctl client report -i " + reportIp + " -p " + reportPort + " && /Plotter create -F " +
 								farmerKey + " -P " + poolKey + " -d /root/rplots/" + userDir + " -t /root/rplots/" + userDir +
-								" -k " + k + " -rp " + reportIp + " -po " + reportPort + " -b 10000"},
+								" -k " + k + " --rp " + reportIp + " --po " + reportPort + " -b 10000"},
 							Resources: corev1.ResourceRequirements{
 								Limits:   limitList,
 								Requests: requestList,
